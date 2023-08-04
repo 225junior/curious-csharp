5ef313707947a487ce88b3bac2accbd6d9a51ae7
225junior <225juniorbrindou@gmail.com>
2023-08-04 16:13:14 +0000

    feat: 02-sum-two-numbers

---
+++ 02-sum-two-numbers/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Entrez un nombre :");
+string firstInput = Console.ReadLine();
+
+Console.WriteLine("Entrez un deuxieme nombre :");
+string secondInput = Console.ReadLine();
+
+if (int.TryParse(firstInput, out int number) && int.TryParse(secondInput, out int number))
+{
+    int firstIput = number;
+}
+else
+{
+    Console.WriteLine("Erreur : Entrée invalide. Veuillez saisir un nombre valide.");
+}
+
+// Console.WriteLine($"Le résultat de {number} * 2 est : {result}");
+
