e15f9c039b437d63b6485168812c87d57a3d3ba8
225junior <225juniorbrindou@gmail.com>
2023-08-05 23:56:39 +0000

    feat: 03-multiplication-table

---
+++ 03-multiplication-table/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("=========== Table de multiplication ========\n\n\n");
+
+Console.Write("Je veux la table de multiplication par: ");
+int.TryParse(Console.ReadLine(), out int a);
+
+Console.Write($"La table de {a} ira de 0 à ");
+int.TryParse(Console.ReadLine(), out int b);
+
+for (int j = 0; j <= b; j++)
+{
+    Console.WriteLine($"{a} x {j} = {a * j}");
+}
