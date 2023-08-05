6d59c3e5aa3eaeb5944b986b73ae26774ff0aadd
225junior <225juniorbrindou@gmail.com>
2023-08-05 23:15:59 +0000

    update 02-sum-two-numbers program

--- 02-sum-two-numbers/Program.cs
+++ 02-sum-two-numbers/Program.cs
@@ -1,18 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿Console.WriteLine("=========== Somme de deux nombres ========\n\n\n");
+
 Console.WriteLine("Entrez un nombre :");
 string firstInput = Console.ReadLine();
 
 Console.WriteLine("Entrez un deuxieme nombre :");
 string secondInput = Console.ReadLine();
 
-if (int.TryParse(firstInput, out int number) && int.TryParse(secondInput, out int number))
+if (int.TryParse(firstInput, out int number1) && int.TryParse(secondInput, out int number2))
 {
-    int firstIput = number;
+    Console.WriteLine($"Le résultat de {number1} + {number2} est : {number1 + number2}");
 }
 else
 {
     Console.WriteLine("Erreur : Entrée invalide. Veuillez saisir un nombre valide.");
 }
 
-// Console.WriteLine($"Le résultat de {number} * 2 est : {result}");
 
