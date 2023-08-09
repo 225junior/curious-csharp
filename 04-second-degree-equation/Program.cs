d632acd88c4a613609811c9a21fe035eb58ad592
225junior <225juniorbrindou@gmail.com>
2023-08-09 23:43:10 +0000

    feat: calcul du discriminant ok

--- 04-second-degree-equation/Program.cs
+++ 04-second-degree-equation/Program.cs
@@ -11,14 +11,14 @@ while (!int.TryParse(Console.ReadLine(), out a))
     Console.WriteLine("Resaisissez le nombre a : ");
 }
 
-Console.WriteLine("Saisissez le nombre b : ");
+Console.Write("Saisissez le nombre b : ");
 while (!int.TryParse(Console.ReadLine(), out b))
 {
     Console.WriteLine("====== Mauvaise saisie de b (nombre entier)! =====\n");
     Console.WriteLine("Resaisissez le nombre b : ");
 }
 
-Console.WriteLine("Saisissez le nombre c : ");
+Console.Write("Saisissez le nombre c : ");
 while (!int.TryParse(Console.ReadLine(), out c))
 {
     Console.WriteLine("====== Mauvaise saisie de c (nombre entier)! =====\n");
@@ -27,4 +27,7 @@ while (!int.TryParse(Console.ReadLine(), out c))
 
 Console.WriteLine("===== Calcul du discriminat ==== \n");
 d = Math.Pow(b, 2) - 4 * a * c;
+
+Console.WriteLine($"discriminat : d = {d}");
+
 
